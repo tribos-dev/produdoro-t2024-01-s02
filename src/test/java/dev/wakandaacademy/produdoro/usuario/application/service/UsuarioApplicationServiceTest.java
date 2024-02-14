98f11da3c3ec9459a237a9511aefda55f1e66c74
Manoel Mathias <dev.mathiasx@gmail.com>
2024-02-14 15:44:00 -0300

    implementa testes unitarios

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,57 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
+    @Mock
+    private UsuarioRepository usuarioRepository;
+    @InjectMocks
+    private UsuarioApplicationService usuarioApplicationService;
 
+    @Test
+    void deveAlterarStatusParaPausaLonga() {
+        Usuario usuario = DataHelper.createUsuario();
+        when(usuarioRepository.salva(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        usuarioApplicationService.alteraStatusParaPausaLonga(usuario.getEmail(), usuario.getIdUsuario());
+        verify(usuarioRepository, times(1)).salva(any());
+    }
+
+    @Test
+    void statusParaPausaLongaFalha() {
+        Usuario usuario = DataHelper.createUsuario();
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        APIException exception = assertThrows(APIException.class, () ->
+                usuarioApplicationService.alteraStatusParaPausaLonga("mathias@gmail.com", UUID.randomUUID()));
+        assertEquals("Credencial de autenticação não é valida", exception.getMessage());
+        assertEquals(HttpStatus.UNAUTHORIZED, exception.getStatusException());
+    }
+
+    @Test
+    void statusJaEstaEmPausaLonga() {
+        Usuario usuario = DataHelper.createUsuario();
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        usuarioApplicationService.alteraStatusParaPausaLonga(usuario.getEmail(), usuario.getIdUsuario());
+        APIException exception = assertThrows(APIException.class,
+                () -> usuarioApplicationService.alteraStatusParaPausaLonga(usuario.getEmail(), usuario.getIdUsuario()));
+        assertEquals("Usuário já está em Pausa Longa.", exception.getMessage());
+        assertEquals(HttpStatus.CONFLICT, exception.getStatusException());
+    }
 }
